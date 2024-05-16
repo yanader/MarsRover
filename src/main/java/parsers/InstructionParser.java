ed08e2a232fbb089ff8e5dfb176f86f8dcf5700b
ste <mrbarrow@hotmail.co.uk>
2024-05-16 13:27:49 +0100

    feat: InstructionParser

--- src/main/java/parsers/InstructionParser.java
+++ src/main/java/parsers/InstructionParser.java
@@ -1,4 +1,33 @@
 package parsers;
 
+import org.example.Instruction;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InstructionParser {
+
+    public static Instruction[] createInstructionList(String input) {
+        if (!createInstructionListInputIsValid(input)) {
+            throw new IllegalArgumentException("Invalid input. L/R/M accepted");
+        }
+        Instruction[] instructionStream = new Instruction[input.length()];
+        for (int i = 0; i < input.length(); i++) {
+            instructionStream[i] = convertCharToInstruction(input.charAt(i));
+        }
+        return instructionStream;
+    }
+
+    private static boolean createInstructionListInputIsValid(String input) {
+        Matcher matcher = Pattern.compile("^[RLM]+$").matcher(input);
+        return matcher.find();
+    }
+
+    private static Instruction convertCharToInstruction(char c) {
+        if (c == 'L') return Instruction.L;
+        if (c == 'R') return Instruction.R;
+        if (c == 'M') return Instruction.M;
+        return null;
+    }
+
 }
--- src/test/java/parsers/InstructionParserTest.java
+++ src/test/java/parsers/InstructionParserTest.java
@@ -1,7 +1,50 @@
 package parsers;
 
+import org.example.Direction;
+import org.example.Instruction;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InstructionParserTest {
 
+    @Test
+    void createInstructionListReturnsInstructionArray() {
+        assertEquals(Instruction[].class, InstructionParser.createInstructionList("input").getClass());
+    }
+
+    @Test
+    void createInstructionListRejectsInvalidInput() {
+        assertAll(() -> {
+           assertThrows(IllegalArgumentException.class, () -> InstructionParser.createInstructionList(""));
+           assertThrows(IllegalArgumentException.class, () -> InstructionParser.createInstructionList("LRF"));
+           assertThrows(IllegalArgumentException.class, () -> InstructionParser.createInstructionList("Q"));
+           assertThrows(IllegalArgumentException.class, () -> InstructionParser.createInstructionList("!"));
+           assertThrows(IllegalArgumentException.class, () -> InstructionParser.createInstructionList("L R M"));
+        });
+    }
+
+    @Test
+    void createInstructionListAcceptsValidInput() {
+        assertAll(() -> {
+            assertDoesNotThrow(() -> InstructionParser.createInstructionList("L"));
+            assertDoesNotThrow(() -> InstructionParser.createInstructionList("R"));
+            assertDoesNotThrow(() -> InstructionParser.createInstructionList("M"));
+            assertDoesNotThrow(() -> InstructionParser.createInstructionList("LMR"));
+            assertDoesNotThrow(() -> InstructionParser.createInstructionList("RMLRLMRMLRLMRMLRMRMRLRMLR"));
+        });
+    }
+
+    @Test
+    void createInstructionListReturnsCorrectList() {
+        Instruction[] testInstructionSetOne = {Instruction.L, Instruction.M, Instruction.R};
+        Instruction[] testInstructionSetTwo = {Instruction.M, Instruction.M, Instruction.M};
+        Instruction[] testInstructionSetThree = {Instruction.M};
+
+        assertAll(() -> {
+           assertArrayEquals(testInstructionSetOne, InstructionParser.createInstructionList("LMR"));
+           assertArrayEquals(testInstructionSetTwo, InstructionParser.createInstructionList("MMM"));
+           assertArrayEquals(testInstructionSetThree, InstructionParser.createInstructionList("M"));
+        });
+    }
 }
