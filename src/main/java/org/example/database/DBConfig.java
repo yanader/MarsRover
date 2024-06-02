ca885a0add82e6c43e12ccd20ef69362c6a42e63
ste <mrbarrow@hotmail.co.uk>
2024-06-02 12:41:27 +0100

    feat: DBConfig

---
+++ src/main/java/org/example/database/DBConfig.java
@@ -0,0 +1,32 @@
+package org.example.database;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class DBConfig {
+    private Properties properties;
+
+    public DBConfig() {
+        properties = new Properties();
+        String configFilePath = "config/db.properties";
+
+        File configFile = new File(configFilePath);
+
+        try {
+            FileInputStream configFileReader = new FileInputStream(configFile);
+            properties.load(configFileReader);
+            configFileReader.close();
+        } catch (IOException e) {
+            System.out.println("File not found");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getURL() {
+        return this.properties.getProperty("db.url");
+    }
+
+}
